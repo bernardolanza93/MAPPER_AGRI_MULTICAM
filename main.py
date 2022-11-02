485c57cec74c7e095db950ff7e1e6b95f2b3987e
bernardolanza93 <bernardo.lanza.tech@gmail.com>
2022-11-02 11:55:19 +0100

    trying to save video dc

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ if enable_T265:
 if SAVE_VIDEO_TIME != 0:
 
     result = cv2.VideoWriter(path_here + '/data/aquisition.avi',
-                             cv2.VideoWriter_fourcc(*'XVID'),
+                             cv2.VideoWriter_fourcc(*'MJPG'),
                              20.0, (1080, 720),1)
 
 
