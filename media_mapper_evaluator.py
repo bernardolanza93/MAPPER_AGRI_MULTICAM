7f7fd54857ee48104b13bf50f2cd46c2277fc8f6
bernardolanza93 <bernardo.lanza.tech@gmail.com>
2022-11-18 18:18:46 +0100

    identification

--- media_mapper_evaluator.py
+++ media_mapper_evaluator.py
@@ -12,7 +12,7 @@ import os
 
 import os
 import sys
-
+import math
 import cv2
 
 
@@ -30,6 +30,11 @@ THRESHOLD = True
 OPENING = True
 PIXEL_COUNTING = True
 THRES_VALUE = 75
+MASK_DEPTH = False
+CONVERT_DEPTH_TO_1CH = False
+CROPPING = True
+MEDIUM_DEPTH_DISPLAY = True
+BLOB_DETECTOR = True
 
 
 BOT = (0, 8, 11)
@@ -40,18 +45,35 @@ print("thres_value = ",THRES_VALUE)
 
 
 def RGBtoD(r, g, b):
-    if r + g + b < 255:
-        return 0
+
     if r >= g and r >= b:
         if g >= b:
             return g - b
         else:
             return (g - b) + 1529
-
     elif g >= r and g >= b:
         return b - r + 510
     elif b >= g and b >= r:
         return r - g + 1020
+
+
+def ColorToD(frame):
+    min_depth = 0
+    max_depth = 10
+    depth_frame = np.zeros((frame.shape[0], frame.shape[1]))
+    div = max_depth - min_depth
+    for i in range(frame.shape[0]):
+        for j in range(frame.shape[1]):
+            r, g, b = frame[i][j][0], frame[i][j][1], frame[i][j][2]
+            hue_value = RGBtoD(r, g, b)
+
+            z_value = (min_depth + ((div) * hue_value) / 1529)
+            depth_frame[i][j] = z_value/255
+            #if z_value != 0:
+
+            #print(z_value/10)
+
+    return depth_frame
 
 
 
@@ -166,66 +188,73 @@ def brightness(img):
         return np.average(img)
 
 
-def blob_detector(im,min_area_blob):
+def blob_detector(im,frame):
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (6, 6))
+    im = cv2.morphologyEx(im, cv2.MORPH_CLOSE, kernel)
     start_time3 = time.time()
 
-    im = cv2.copyMakeBorder(im, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=[255,255,255])
 
 
+    edge = cv2.Canny(im, 175, 175)
+
+    contours, hierarchy = cv2.findContours(edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
 
+    # Find the index of the largest contour
+    valid = []
+    color = 0
+
+    for cnt in contours:
+        #area
+        area = cv2.contourArea(cnt)
+        #perimeter
+        perimeter = cv2.arcLength(cnt, True)
+        #fitting a line
+
+        if perimeter != 0 and area != 0:
+            circularity = (4 * math.pi * area ) / (pow(perimeter,2))
+
+            if perimeter > 1000:
+                if circularity < 0.01:
+                    if area > 15:
 
 
+                        print("_____________")
+                        print(circularity)
+
+                        print("area and perim", int(area), int(perimeter))
+                        cv2.drawContours(frame, [cnt], 0, (0 + 3*color, 255 - 3*color, 0+ 3*color), 3)
+
+                        rows, cols = frame.shape[:2]
+                        [vx, vy, x, y] = cv2.fitLine(cnt, cv2.DIST_L2, 0, 0.01, 0.01)
+                        lef = int((-x * vy / vx) + y)
+
+                        ri = int(((cols - x) * vy / vx) + y)
+                        cv2.line(frame, (cols - 1, ri), (0, lef), (0, 255, 0), 2)
+                        #x, y, w, h = cv2.boundingRect(cnt)
+                        #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                        epsilon = 0.1 * cv2.arcLength(cnt, True)
+                        approx = cv2.approxPolyDP(cnt, epsilon, True)
+                        rect = cv2.minAreaRect(cnt)
+                        box = cv2.boxPoints(rect)
+                        box = np.int0(box)
+                        cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
+                        print(box)
+                        color += 10
+                        if False:
+                            up = 0 + 50
+                            down = frame_height - 50
+                            left = 0 + 300
+                            right = frame_width - 300
 
 
-    # Setup SimpleBlobDetector parameters.
-    params = cv2.SimpleBlobDetector_Params()
-
-    # Change thresholds
-    #params.minThreshold = 10;
-    #params.maxThreshold = 200;
-
-    # Filter by Area.
-    params.filterByArea = 0
-    params.minArea = 300
-
-    # Filter by Circularity
-    params.filterByCircularity = True
-    params.minCircularity = 0.001
-
-    # Filter by Convexity
-    params.filterByConvexity = True
-    params.minConvexity = 0.001
-
-    # Filter by Inertia
-    params.filterByInertia = True
-    params.minInertiaRatio = 0.001
+                            frame = frame[minx:maxx, miny:maxy]
 
 
+                            frame = frame[top_left_y:bot_right_y, top_left_x:bot_right_x]
 
-    detector = cv2.SimpleBlobDetector_create(params)
-
-    keypoints = detector.detect(im)
-    total_count = 0
-
-    im = cv2.cvtColor(im, cv2.COLOR_GRAY2RGB)
-
-    if len(keypoints)>1:
-
-        for keyPoint in keypoints:
-            total_count = total_count + 1
-            x = keyPoint.pt[0]
-            y = keyPoint.pt[1]
-            s = keyPoint.size
-
-            #print("size :",s)
-            if s > min_area_blob:
-                cv2.circle(im, (int(x), int(y)), int(s), (0,255,0), 2)
-
-            else:
-                cv2.circle(im, (int(x), int(y)), int(s), (0, 0, 255), 2)
-
-
+    return im,frame,edge
     # Draw detected blobs as red circles.
     # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
     #im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0, 0, 255),
@@ -282,150 +311,192 @@ high_V_name = 'High V'
 
 for file in os.listdir(PATH_HERE + PATH_2_AQUIS):
     print("files:", os.listdir(PATH_HERE + PATH_2_AQUIS))
+
     if file.endswith(".mkv"):
-        path= os.path.join(PATH_HERE + PATH_2_AQUIS, file)
-        video = cv2.VideoCapture(path)
+        print(file.split(".")[0])
+
+        if file.split(".")[0] == "RGB":
+
+            path_rgb= os.path.join(PATH_HERE + PATH_2_AQUIS, file)
+            video1 = cv2.VideoCapture(path_rgb)
+        elif file.split(".")[0] == "DEPTH":
+
+            path_depth= os.path.join(PATH_HERE + PATH_2_AQUIS, file)
+            video2 = cv2.VideoCapture(path_depth)
 
 
-        # We need to check if camera
-        # is opened previously or not
-        if (video.isOpened() == False):
-            print("Error reading video file")
 
-        # We need to set resolutions.
-        # so, convert them from float to integer.
-        frame_width = int(video.get(3))
-        frame_height = int(video.get(4))
+# We need to check if camera
+# is opened previously or not
+if (video1.isOpened() == False):
+    print("Error reading video rgb")
+if (video2.isOpened() == False):
+    print("Error reading video depth")
+# We need to set resolutions.
+# so, convert them from float to integer.
 
-        size = (frame_width, frame_height)
+frame_width = int(video1.get(3))
+frame_height = int(video1.get(4))
+frame_width2 = int(video2.get(3))
+frame_height2 = int(video2.get(4))
+print("height : ", frame_height)
+print("width : ", frame_width)
 
-        # Below VideoWriter object will create
-        # a frame of above defined The output
-        # is stored in 'filename.avi' file.
+size = (frame_width, frame_height)
+size2 = (frame_width2, frame_height2)
+
+# Below VideoWriter object will create
+# a frame of above defined The output
+# is stored in 'filename.avi' file.
+if SAVE_VIDEO:
+    result = cv2.VideoWriter('filename.avi',
+                             cv2.VideoWriter_fourcc(*'MJPG'),
+                             20, size)
+if TRACKBAR:
+    cv2.namedWindow(window_capture_name)
+    cv2.namedWindow(window_detection_name)
+
+    cv2.createTrackbar(low_H_name, window_detection_name, low_H, max_value_H, on_low_H_thresh_trackbar)
+    cv2.createTrackbar(high_H_name, window_detection_name, high_H, max_value_H, on_high_H_thresh_trackbar)
+    cv2.createTrackbar(low_S_name, window_detection_name, low_S, max_value, on_low_S_thresh_trackbar)
+    cv2.createTrackbar(high_S_name, window_detection_name, high_S, max_value, on_high_S_thresh_trackbar)
+    cv2.createTrackbar(low_V_name, window_detection_name, low_V, max_value, on_low_V_thresh_trackbar)
+    cv2.createTrackbar(high_V_name, window_detection_name, high_V, max_value, on_high_V_thresh_trackbar)
+
+
+
+while (True):
+    ret, frame = video1.read()
+    ret2, frame2 = video2.read()
+
+    if ret == True and ret2 == True:
+
+        # Write the frame into the
+        # file 'filename.avi'
         if SAVE_VIDEO:
-            result = cv2.VideoWriter('filename.avi',
-                                     cv2.VideoWriter_fourcc(*'MJPG'),
-                                     20, size)
 
-        cv2.namedWindow(window_capture_name)
-        cv2.namedWindow(window_detection_name)
+            result.write(frame)
 
-        cv2.createTrackbar(low_H_name, window_detection_name, low_H, max_value_H, on_low_H_thresh_trackbar)
-        cv2.createTrackbar(high_H_name, window_detection_name, high_H, max_value_H, on_high_H_thresh_trackbar)
-        cv2.createTrackbar(low_S_name, window_detection_name, low_S, max_value, on_low_S_thresh_trackbar)
-        cv2.createTrackbar(high_S_name, window_detection_name, high_S, max_value, on_high_S_thresh_trackbar)
-        cv2.createTrackbar(low_V_name, window_detection_name, low_V, max_value, on_low_V_thresh_trackbar)
-        cv2.createTrackbar(high_V_name, window_detection_name, high_V, max_value, on_high_V_thresh_trackbar)
+        if CROPPING:
+            up = 0 + 50
+            down = frame_height -50
+            left = 0 +300
+            right = frame_width -300
+            frame = frame[up:down, left:right]
+            frame2 = frame2[up:down, left:right]
 
 
-
-        while (True):
-            ret, frame = video.read()
-
-            if ret == True:
-
-                # Write the frame into the
-                # file 'filename.avi'
-                if SAVE_VIDEO:
-
-                    result.write(frame)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #th2 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+        #th3 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
+        #frame_HSV = frame
 
-                #th2 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-                #th3 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+        #sistemi di mask generation
+        if TRACKBAR:
+            mask = cv2.inRange(frame_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
+        elif THRESHOLD:
+            ret, mask = cv2.threshold(gray, THRES_VALUE, 255, cv2.THRESH_BINARY)
+        #frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-                #frame_HSV = frame
+        #mask = cv2.inRange(frame_HSV, BOT, TOP)
 
-                #sistemi di mask generation
-                if TRACKBAR:
-                    mask = cv2.inRange(frame_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
-                elif THRESHOLD:
-                    ret, mask = cv2.threshold(gray, THRES_VALUE, 255, cv2.THRESH_BINARY)
-                #frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-                #mask = cv2.inRange(frame_HSV, BOT, TOP)
-
-                if OPENING:
-                    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
-                    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        if OPENING:
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
+            mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 
 
 
 
+        imask = mask < 255
+        imagem = (255 - mask)
+        green = 255 * np.ones_like(frame, np.uint8)
+        green[imask] = frame[imask]  # dentro i mask metto frame
+        if MASK_DEPTH:
+            frame22 = 255 * np.ones_like(frame2, np.uint8)
+            frame22[imask] = frame2[imask]
+
+        else:
+            frame22 = frame2
+        if CONVERT_DEPTH_TO_1CH:
+            frame22 = ColorToD(frame22)
 
 
+        if PIXEL_COUNTING:
+            org = (100, 100)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+
+            # fontScale
+            fontScale = 1
+
+            # Blue color in BGR
+            color = (255, 0, 0)
+
+            # Line thickness of 2 px
+            thickness = 2
+            number_of_black_pix = np.sum(mask == 0)
+            image = cv2.putText(green, 'pix : ' + str(number_of_black_pix), org, font,
+                                fontScale, color, thickness, cv2.LINE_AA)
+        #Green,Imagen = mask_generation(frame,BOT,TOP)
+        #Imagen = cv2.bitwise_not(Imagen)
+        #Imagen = undesired_objects(Imagen)
+
+        # Display the frame
+        # saved in the file
+        #maskedcolor = resize_image(Green,50)
+        #mask = resize_image(Imagen,50)
+        #cv2.imshow('Frame', maskedcolor)
+        if CONVERT_DEPTH_TO_1CH:
+            if MEDIUM_DEPTH_DISPLAY:
+                print(frame22)
+                #medium = frame22()
+        if BLOB_DETECTOR:
+            mask,frame,edge = blob_detector(mask, frame)
+
+        dimension = 50
+        edge = resize_image(edge,dimension)
+        frame = resize_image(frame,dimension)
+        green = resize_image(green, dimension)
+        mask = resize_image(mask, dimension)
+        frame22 = resize_image(frame22, dimension)
 
 
-                imask = mask < 255
-                imagem = (255 - mask)
-                green = 255 * np.ones_like(frame, np.uint8)
-                green[imask] = frame[imask]  # dentro i mask metto frame
+        #cv2.imshow('fff', frame)
+        #cv2.imshow('ff', mask)
+        #fig,lineR,lineG,lineB = make_hinstogram_base_plt()
+        #animate_plot_data(frame, fig, lineR,lineG,lineB)
 
+        # Press S on keyboard
+        # to stop the process
+        #                                         time.sleep(0.1)
+        #if cv2.waitKey(1) & 0xFF == ord('s'):
+            #break
+        cv2.imshow("or", frame)
+        cv2.imshow("mask", mask)
+        cv2.imshow("green", green)
+        cv2.imshow("bella", frame22)
+        cv2.imshow("belhla", edge)
 
-                if PIXEL_COUNTING:
-                    org = (100, 100)
-                    font = cv2.FONT_HERSHEY_SIMPLEX
+        key = cv2.waitKey(0)
+        if key == ord('q') or key == 27:
+            sys.exit()
+            break
 
-                    # fontScale
-                    fontScale = 1
+    # Break the loop
+    else:
+        break
 
-                    # Blue color in BGR
-                    color = (255, 0, 0)
+# When everything done, release
+# the video capture and video
+# write objects
+video.release()
+if SAVE_VIDEO:
+    result.release()
 
-                    # Line thickness of 2 px
-                    thickness = 2
-                    number_of_black_pix = np.sum(mask == 0)
-                    image = cv2.putText(green, 'pix : ' + str(number_of_black_pix), org, font,
-                                        fontScale, color, thickness, cv2.LINE_AA)
-                #Green,Imagen = mask_generation(frame,BOT,TOP)
-                #Imagen = cv2.bitwise_not(Imagen)
-                #Imagen = undesired_objects(Imagen)
-
-                # Display the frame
-                # saved in the file
-                #maskedcolor = resize_image(Green,50)
-                #mask = resize_image(Imagen,50)
-                #cv2.imshow('Frame', maskedcolor)
-                dimension = 50
-                frame = resize_image(frame,dimension)
-                green = resize_image(green, dimension)
-                mask = resize_image(mask, dimension)
-
-
-                #cv2.imshow('fff', frame)
-                #cv2.imshow('ff', mask)
-                #fig,lineR,lineG,lineB = make_hinstogram_base_plt()
-                #animate_plot_data(frame, fig, lineR,lineG,lineB)
-
-                # Press S on keyboard
-                # to stop the process
-                #                                         time.sleep(0.1)
-                #if cv2.waitKey(1) & 0xFF == ord('s'):
-                    #break
-                cv2.imshow(window_capture_name, mask)
-                cv2.imshow(window_detection_name, green)
-
-                key = cv2.waitKey(0)
-                if key == ord('q') or key == 27:
-                    sys.exit()
-                    break
-
-            # Break the loop
-            else:
-                break
-
-        # When everything done, release
-        # the video capture and video
-        # write objects
-        video.release()
-        if SAVE_VIDEO:
-            result.release()
-
-        # Closes all the frames
-        cv2.destroyAllWindows()
+# Closes all the frames
+cv2.destroyAllWindows()
 
 
 sys.exit()
