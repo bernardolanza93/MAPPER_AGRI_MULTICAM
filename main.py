6c812a40ac823ed0e28543d9d42f29f35381af73
bernardolanza93 <bernardo.lanza.tech@gmail.com>
2022-11-24 16:46:09 +0100

    function file, volume calculation alpha, skeleton

--- main.py
+++ main.py
@@ -162,8 +162,8 @@ if SAVE_VIDEO_TIME != 0:
 
     try:
 
-        gst_out_depth = "appsrc ! video/x-raw, format=BGR ! queue ! videoconvert ! video/x-raw,format=BGRx ! nvvidconv ! nvv4l2h264enc ! h264parse ! matroskamux ! filesink location=DEPTH.mkv "
-
+        #gst_out_depth = "appsrc ! video/x-raw, format=BGR ! queue ! videoconvert ! video/x-raw,format=BGRx ! nvvidconv ! nvv4l2h264enc ! h264parse ! matroskamux ! filesink location=DEPTH.mkv "
+        gst_out_depth = ('gst-launch-1.0 rtspsrc location=rtsp://ip/url ! rtph264depay ! h264parse ! mp4mux ! filesink location=file.mp4')
         #gst_out_depth = ('appsrc caps=video/x-raw,format=GRAY8,width=1920,height=1080,framerate=30/1 ! ''videoconvert ! omxh265enc ! video/x-h265, stream-format=byte-stream ! ''h265parse ! filesink location=test.h265 ')
         out_depth = cv2.VideoWriter(gst_out_depth, cv2.CAP_GSTREAMER,  20.0, (1920, 1080))
 
@@ -231,8 +231,9 @@ while True:
             try:
                 out.write(color_image)
                 try:
-                    #save here depth map
-                    np.savetxt("image.txt", depth_image,fmt='%i')
+                    #save here depth mapù
+                    out_depth.write(depth_image)
+                    #np.savetxt("image.txt", depth_image,fmt='%i')
 
                 except Exception as e:
                     print("error saving depth 1 ch:||||:: %s", str(e))
