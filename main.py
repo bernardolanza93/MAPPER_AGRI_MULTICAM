8f8bb2e4a47b0ddd85f0d1063f8c32a4f1a7785b
bernardolanza93 <bernardo.lanza.tech@gmail.com>
2022-12-01 12:38:44 +0100

    function file, volume calculation alpha, skeleton

--- main.py
+++ main.py
@@ -176,7 +176,7 @@ if SAVE_VIDEO_TIME != 0:
     out = cv2.VideoWriter(gst_out, cv2.CAP_GSTREAMER,  20.0, (1920, 1080))
 
     try:
-        gst_out_depth   = "appsrc ! video/x-raw, format=GRAY ! queue ! videoconvert ! video/x-raw,format=GRAY ! nvvidconv ! nvv4l2h264enc ! h264parse ! matroskamux ! filesink location=DEPTH.mkv "
+        gst_out_depth   = "appsrc ! video/x-raw, format=GRAY ! video/x-raw,format=GRAY ! nvvidconv ! nvv4l2h264enc ! h264parse ! matroskamux ! filesink location=DEPTH.mkv "
         #gst_out_depth = "appsrc ! video/x-raw, format=GRAY8 ! filesink location=DEPTH.mkv "
         #gst_out_depth = ("appsrc ! autovideoconvert ! omxh265enc ! matroskamux ! filesink location=test.mkv" )
         #gst_out_depth = ('appsrc caps=video/x-raw,format=GRAY8,width=1920,height=1080,framerate=30/1 ! ''videoconvert ! omxh265enc ! video/x-h265, stream-format=byte-stream ! ''h265parse ! filesink location=test.h265 ')
