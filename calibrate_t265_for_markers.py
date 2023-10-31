82aff664e6185bea92d89c98dca0957037fcd6f6
bernardolanza93 <bernardo.lanza.tech@gmail.com>
2023-10-31 17:10:44 +0100

    preparato studenti alpha

--- calibrate_t265_for_markers.py
+++ calibrate_t265_for_markers.py
@@ -190,7 +190,7 @@ if __name__ == "__main__":
             print("CALIBRATION ALREADY COMPLETED")
         else:
             print("CALIBRATION STARTING....")
-            calibrate()
+            calibrate_v2()
     else:
         print("Error: Only", jpg_count ,"JPG images found in the folder. You need at least 20.")
         print("CAPTURING....")
