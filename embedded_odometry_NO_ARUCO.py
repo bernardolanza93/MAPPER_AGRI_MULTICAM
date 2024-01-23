be0acb993aa237497d1be530dcd67c08ddff6a83
bernardolanza93 <bernardo.lanza.tech@gmail.com>
2024-01-23 15:51:05 +0100

    update on usb

--- embedded_odometry_NO_ARUCO.py
+++ embedded_odometry_NO_ARUCO.py
@@ -138,9 +138,8 @@ def odometry_capture_no_aruco(global_status):
                         # print("Acceleration: {}\n".format(data.acceleration))
 
                         writeCSVdata_odometry("_NO_ARUCO_" +timing_abs, pose_list)
-                        if not enable_D435i:
-                            # converte la velocita di salvataggio dai 1500 FPS (T265 standalone)  ad un acquisizione piu realistica (15 FPS della D435)
-                            time.sleep(DIVIDER_FPS_REDUCTION)
+
+                        time.sleep(DIVIDER_FPS_REDUCTION)
                     local_status = global_status.value
                     if local_status == 0:
                         print("TERMINATION SIGNAL DETECTED")
